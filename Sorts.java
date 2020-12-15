1f009185296eccc9cf06c4d330cccf5af0dd16b2
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-15 14:22:36 -0500

    initial code

--- Sorts.java
+++ Sorts.java
@@ -4,5 +4,24 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  public static void bubbleSort(int[] data){
+    int n = data.length;
+    boolean noswap = true;
+    while(noswap){
+      for (int i = 0; i<n-1;i++){
+        int temp = 0;
+        if (data[i]>data[i+1]){
+          temp = data[i];
+          data[i]=data[i+1];
+          data[i+1]=temp;
+          noswap = false;
+        }
+      }
+      n--;
+      if (noswap){
+        break;
+      }
+    }
+    System.out.println(Arrays.toString(data));
+  }
 }
