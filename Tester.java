4a83968a07013df8a19b57020786d0473190cd53
OliverSantiago <osantiagokermani20@stuy.edu>
2020-12-16 14:28:06 -0500

    New tests

--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[]args){
-    int[] data = new int[]{1,2,3,4,5};
-    Sorts.bubbleSort(data);
+    // int[] data = new int[]{1,2,3,4,5};
+    // Sorts.bubbleSort(data);
+    // Good so far
+    int[] data2 = new int[]{5,4,3,2,1};
+    Sorts.bubbleSort(data2);
   }
 }
